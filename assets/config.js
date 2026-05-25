5c1ac2a9416750d239edf3582351d15486044b9f
LG <lusavkaedu@gmail.com>
2026-05-25 12:38:30 +0100

    Point dashboard at new playlist PL6g-DxT2XMgI16gDc2-FG8nOT9k-g1SUt

--- assets/config.js
+++ assets/config.js
@@ -1,4 +1,4 @@
   window.CONFIG = {
       CLIENT_ID: '175213838358-pr5d1d00ov12g4usd9ip289ijjshvt48.apps.googleusercontent.com',
-      PLAYLIST_ID: 'PL6g-DxT2XMgJ5kSZX-_7oCa-31OyNSj6S',
+      PLAYLIST_ID: 'PL6g-DxT2XMgI16gDc2-FG8nOT9k-g1SUt',
   }
